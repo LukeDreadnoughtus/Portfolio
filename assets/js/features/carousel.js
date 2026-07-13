d3fd493fa6af10f78aa23bcf311175ce81fd4de0
LukeDreadnoughtus <emo19uchiha97lukas@gmail.com>
2026-07-13 18:05:22 +0200

    count quote index and load Carousel-card

---
+++ assets/js/features/carousel.js
@@ -0,0 +1,15 @@
+import{state,quotes}from'../data/content.js';
+import{qs,qsa,setHtml}from'../utils/dom.js';
+import{carouselTemplate}from'../templates/carouselTemplates.js';
+
+const animationTime=700;
+
+
+export const initCarousel=()=>{
+  qsa('[data-quote]').forEach(connectButton);
+  restartAutoSlide();
+};
+
+const connectButton=button=>{
+  button.onclick=()=>scrollCarousel(button.dataset.quote);
+};
