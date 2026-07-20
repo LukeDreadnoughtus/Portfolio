52387ef2733dd48e9b75800cc99eb7a3d2bbf371
LukeDreadnoughtus <emo19uchiha97lukas@gmail.com>
2026-07-20 19:17:31 +0200

    dialog Templates

--- assets/js/templates/dialogTemplates.js
+++ assets/js/templates/dialogTemplates.js
@@ -13,3 +13,29 @@ const iconFile = name => name === 'Firebase'
     : name === 'HTML'
       ? 'HTML.svg'
       : 'JavaScript.svg';
+
+      const skill = name => `
+<span class="used-skills__item karla">
+  <img class="used-skills__icon"
+    src="assets/img/dialog/${iconFile(name)}"
+    alt="${name}">
+  ${name}
+</span>`;
+
+export const projectDialogTemplate = index => {
+  const project = projects[index];
+  return `
+<div class="project-dialog__card" role="dialog"
+  aria-modal="true">
+  ${closeButton()}
+  <div class="project-dialog__wrapper">
+    <div class="project-dialog__grid">
+      ${dialogInfo(project, index)}
+      ${dialogImage(project)}
+    </div>
+    <button class="next-project karla">
+      ${t().nextProject}
+    </button>
+  </div>
+</div>`;
+};
