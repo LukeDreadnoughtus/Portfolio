839c4ca80fcb16d803ac9fe567aa74ebf4128903
LukeDreadnoughtus <emo19uchiha97lukas@gmail.com>
2026-07-26 07:36:44 +0200

    form validation

--- assets/js/features/contacts.js
+++ assets/js/features/contacts.js
@@ -3,3 +3,10 @@ import { state, i18n } from '../data/content.js';
 
 const t = () => i18n[state.lang];
 const fieldNames = ['name', 'email', 'message'];
+
+const bindFormEvents = form => {
+  fieldNames.forEach(name => bindTextField(form[name]));
+  form.privacy.addEventListener('change', () => {
+    validatePrivacy(form.privacy, true);
+  });
+};
